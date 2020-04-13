73fee607f9a573c6490918cb536a3b4b478294ae
Zhekuson <zhekuson@gmail.com>
2020-04-13 16:25:24 +0300

    added auth controller

---
+++ CodequizApi/Features/Auth/AuthController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodequizApi.Features.Auth
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController:Controller
+    {
+        [HttpPost("token")]
+        public IActionResult GetToken()
+        {
+
+        }
+    }
+}
