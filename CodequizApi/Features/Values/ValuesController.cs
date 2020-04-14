af819ce5fdcae51be8d8ecab47901b904c4e4afe
Zhekuson <zhekuson@gmail.com>
2020-04-14 17:31:55 +0300

    deleted dependency aspnetapp

--- CodequizApi/Features/Values/ValuesController.cs
+++ CodequizApi/Features/Values/ValuesController.cs
@@ -9,7 +9,6 @@ namespace CodequizApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize]
     public class ValuesController : ControllerBase
     {
         // GET api/values
